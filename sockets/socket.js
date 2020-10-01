e3a79997c06a8788ebccd69e76ab53c5e61e67ca
Davis Álvarez <alvarez.davis@gmail.com>
2020-10-01 13:59:24 -0500

    Se agregan modelos

--- sockets/socket.js
+++ sockets/socket.js
@@ -1,16 +1,32 @@
 const { io } = require('../index');
+const Band = require('../models/band');
+const Bands = require('../models/bands');
+
+const bands = new Bands();
+
+bands.addBand( new Band( 'Queen' ) );
+bands.addBand( new Band( 'Bon Jovi' ) );
+bands.addBand( new Band( 'Metallica' ) );
+
 
 //Sockets Messages
 io.on( 'connection', client => {
     console.log( 'Cliente conectado' );
 
-    client.on('disconnect', () => { 
+    client.emit( 'active-bands', bands.getBands() );
 
+    client.on('disconnect', () => { 
         console.log( 'Cliente desconectado' );
     });
 
-    client.on('mensaje', ( payLoad ) =>{
-        console.log( 'Mensaje', payLoad);
-        io.emit( 'mensaje', { admin: 'Nuevo mensaje' } )
+    client.on('vote-band', ( payLoad ) =>{
+        bands.voteBand( payLoad.id );
+        io.emit( 'active-bands', bands.getBands() );
+    });
+
+    client.on('emitir-mensaje', ( payLoad ) =>{
+        //console.log( 'Mensaje a enviar', payLoad);
+        //io.emit( 'nuevo-mensaje', payLoad);
+        client.broadcast.emit( 'nuevo-mensaje', payLoad);
     });
 });
